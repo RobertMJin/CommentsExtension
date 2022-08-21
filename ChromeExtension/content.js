7355202682e74e216fd7fbf24fe2e7945e478707
Robert Jin <robert-jin@outlook.com>
2022-08-21 02:00:51 -0400

    updates on connecting popup to content scripts

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,4 +1,31 @@
-
 chrome.runtime.onMessage.addListener(
-    callback: function,
-  )
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ? "from a content script:" + sender.tab.url :"from the extension");
+      if (request.greeting === "Likes button") {
+        sendResponse({farewell: "Like received"});
+      } else if (request.greeting === "Replies button") {
+        sendResponse({farewell: "Reply received"});
+      }
+    }
+  );
+
+chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+var url = tabs[0].url;
+url = url.substring(32, 43);
+console.log(url);
+});
+
+
+chrome.runtime.getPlatformInfo(function(info) {
+    var os = info.os;
+ });
+if (os === "win") {
+    var run=new ActiveXObject('WSCRIPT.Shell').Run("gradlew run --args="+url+"replies");
+} else if (os === "mac") {
+    console.log("Currently Unsupported OS!");
+} else {
+    console.log("Currently Unsupported OS!");
+}
+
+
+// sends url to other java code (main)
--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -4,7 +4,12 @@ function mostLiked(){
     var url = tabs[0].url;
     console.log("Url " + url);
   });
-  chrome.runtime.sendMessage(currentWindow, "Likes Mode");
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "Likes button"}, function(response) {
+      console.log(response.farewell);
+    });
+  });
   
 }
 
